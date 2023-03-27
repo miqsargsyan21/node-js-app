d0929bf49ef1df0fe87a83d3b70076637fc7f4e9
Michael Sargsyan <mikayel.sargsyan@mamble.co>
2023-03-27 12:16:49 +0400

    Reverted to default

--- routers/deleterRouter.js
+++
@@ -1,8 +0,0 @@
-const {deleteItem} = require('../controllers/deleterControllers')
-const Router = require('../services/RouteService')
-
-const router = new Router()
-
-router.delete('/delete/:id', deleteItem)
-
-module.exports = router
