8eda54be6789807d5214e671574c2b7da8a6e502
Michael Sargsyan <mikayel.sargsyan@mamble.co>
2023-03-27 12:15:34 +0400

    Reverted to default

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const dotenv = require('dotenv')
-const http = require('http')
-const handleRoutes = require('./routes/index')
-
-const {
-    HOST: host,
-    PORT: port
-} = dotenv.config().parsed
-
-const server = http.createServer((req, res) => {
-    handleRoutes(req, res)
-})
-
-server.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`)
-})
+console.log('Hello World!')
